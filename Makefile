48ba76a6a76225ef357d4d67fb9bd3c3375c6d24
allen0127 <allen0127@naver.com>
2024-03-17 06:19:35 -0400

    After reading binary file test

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 all: add_nbo
 
+add_nbo: main.o
+	g++ -o add_nbo main.o
+
 main.o: main.c
 	g++ -g -c -o main.o main.c
-
-add_ndo: main.o
-	g++ -o add_nbo main.o
 
 clean:
 	rm -f add_nbo *.o
--- main.c
+++ main.c
@@ -2,16 +2,20 @@
 #include <stdint.h>
 
 int main(int argc, char* argv[]){
-	if(argc!=2) return 0;
-	FILE* fp1=fopen(argv[0], "rb");
-	FILE* fp2=fopen(argv[1], "rb");
-	uint32_t* num[2];
-	size_t count=fread(num, sizeof(uint32_t), 1, fp1);
+	if(argc!=3) {
+		printf("Error1");
+		return 0;
+	}
+	FILE* fp1=fopen(argv[1], "rb");
+	FILE* fp2=fopen(argv[2], "rb");
+	uint32_t num1[1];
+	uint32_t num2[1];
+	size_t count=fread(num1, sizeof(uint32_t), 1, fp1);
 	if(count<1){
-		printf("Error");
+		printf("Error2");
 	}
 	else{
-	printf("%x",num[1]);
+	printf("%x",num1[0]);
 	}
 	fclose(fp1), fclose(fp2);
 	return 0;
